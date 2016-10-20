d328c01a78adc7e6dc9d1bfe9e343ba9b3e23e30
Zachary Trudo <lockout87@gmail.com>
2016-10-20 00:56:07 -0700

    MVP has been reached.
    Everything is working, I've reached a minimum viable product and can present
    it to Prof. Norris.

--- docker-images/jenkins/build.py
+++ docker-images/jenkins/build.py
@@ -5,10 +5,12 @@ import subprocess
 import sys
 from shutil import copyfile
 
+
 def arg_parse():
   parser = argparse.ArgumentParser()
   parser.add_argument('-v', '--version', dest='version', default='latest')
   return parser.parse_args()
+
 
 def setup_credentials():
   credential_config = os.path.abspath("config/credentials.xml")
@@ -32,12 +34,50 @@ def setup_credentials():
   copyfile("tmp.xml", credential_config)
   os.remove("tmp.xml")
 
+
+def setup_user(user_name):
+  match = "{{{{ {usr_nm}_hash }}}}".format(usr_nm=user_name)
+  hash_file = "/root/certs/jenkins/users/{usr_nm}_hash".format(usr_nm=user_name)
+  user_file = "config/users/{usr_nm}/config.xml".format(usr_nm=user_name)
+  hash_value = ""
+  with open(hash_file, "r") as hsh:
+    hash_value = hsh.read().rstrip()
+  with open(user_file, "r") as file_in:
+    with open("tmp.xml", "w") as file_out:
+      for line in file_in:
+        if match in line:
+          line = line.replace(match, hash_value)
+          print line
+        file_out.write(line)
+  copyfile("tmp.xml", user_file)
+  os.remove("tmp.xml")
+
+ 
+def setup_jjb():
+  match = "{{ hpcl_admin_password }}"
+  password_file = "/root/certs/jenkins/users/hpcl_admin"
+  jjb_conf = "config/jjb.conf"
+  password = ""
+  with open(password_file, "r") as psswd:
+    password = psswd.read().rstrip()
+  with open(jjb_conf, "r") as jjb:
+    with open("tmp", "w") as out_file:
+      for line in jjb:
+        if match in line:
+          line = line.replace(match, password)
+        out_file.write(line)
+  copyfile("tmp", jjb_conf)
+  os.remove("tmp")
+
+
 def build_container(args):
   subprocess.call("docker build -t hpcl/jenkins:{vrsn} .".format(vrsn=args.version), shell=True)
 
 def main():
   args = arg_parse()
   setup_credentials()
+  setup_user("hpcl_admin")
+  setup_jjb()
   build_container(args)
 
 
